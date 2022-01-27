205fbf79077e1f9d2b02840d6d567244c37e88b4
DJrOf <danieljrofosu@gmail.com>
2022-01-27 18:07:05 +0100

    updatescript

--- js/script.js
+++ js/script.js
@@ -5,35 +5,33 @@ Vue.config.devtools = true;
 const app = new Vue({
     el: '#root',
 
-  data() {
+  data: {
       //L'array di immagini è in una funzione che restituisce le singole immagini.
-    return {
       images: [
         "img/image1.jpg",
         "img/image2.jpg",
         "img/image3.jpg",
-        "img/image4.jpg"
+        "img/image4.jpg",
       ],
       // Determinare la posizione corrente sul valore di origine. 
       currentIndex: 0,
-    };
-  },
+    },
+
   methods: {
-// Funzioni che modificano la posizione d'origine (currentIndex) degli elementi dell'array (slide left/right)
-    next: function() {
-      this.currentIndex += 1;
-    },
-    prev: function() {
-      this.currentIndex -= 1;
-    },
-   
-  },
-  currentImg: function() {
-    return this.images[Math.abs(this.currentIndex) % this.images.length];
-  /* Math.abs restituisce il valore assoulto di un numero, 
-  ed assume valore "false" quando questo è negativo.
-  Tramite questa funzione si regola il 
-  posizionamento del currentIndex all'interno dell'array.
-  */
-},
+    // Funzioni che modificano la posizione d'origine (currentIndex) degli elementi dell'array (slide left/right)
+        next() {
+          this.currentIndex += 1;
+        },
+        prev() {
+          this.currentIndex -= 1;
+        },
+        currentImg() {
+            return this.images[Math.abs(this.currentIndex) % this.images.length];
+          /* Math.abs restituisce il valore assoulto di un numero, 
+          ed assume valore "false" quando questo è negativo.
+          Tramite questa funzione si regola il 
+          posizionamento del currentIndex all'interno dell'array.
+          */
+        },
+      },
 })
